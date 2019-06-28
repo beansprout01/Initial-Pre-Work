bc2da6dcbf213daf88d64ad4b4cf8491081dea52
beansprout01 <benjaminl.sproul@gmail.com>
2019-06-28 15:25:02 -0600

    Code for levels 11-15 in ES6 with comments on what I learned.

---
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,9 @@
+//I learned to use the spread operator to unpack arrays into a new array
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(arr2);
---
+++ 6-Introduction-to-the-ES6-Challenges/14-Use-Destructuring-Assignment-to-Assign-Variables-from-Arrays.js
@@ -0,0 +1,11 @@
+//I learned how to use destructuring assignment to assign variables values of arrays
+
+let a = 8, b = 6;
+(() => {
+  "use strict";
+  // change code below this line
+  [b,a] = [a,b];
+  // change code above this line
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
